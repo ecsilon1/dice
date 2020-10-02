5743381a24179c643c9d1d08b8598b6d193f53f6
ecsilon1 <61433798+ecsilon1@users.noreply.github.com>
2020-10-02 22:50:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,46 +3,46 @@
  var randomNumber=Math.floor(Math.random(randomNumber)*6)+1;
 
 if(randomNumber===1){
-  document.querySelector(".img1").setAttribute("src" , "https://github.com/ecsilon1/dice/blob/main/images/dice1.png");
+  document.querySelector(".img1").setAttribute("src" , "images/dice1.png");
 }
 
  else if(randomNumber===2){
-  document.querySelector(".img1").setAttribute("src" , "https://github.com/ecsilon1/dice/blob/main/images/dice2.png");
+  document.querySelector(".img1").setAttribute("src" , "images/dice2.png");
 }
  else if (randomNumber===3){
-  document.querySelector(".img1").setAttribute("src" , "https://github.com/ecsilon1/dice/blob/main/images/dice3.png");
+  document.querySelector(".img1").setAttribute("src" , "images/dice3.png");
 }
  else if (randomNumber===4){
-  document.querySelector(".img1").setAttribute("src" ,"https://github.com/ecsilon1/dice/blob/main/images/dice4.png");
+  document.querySelector(".img1").setAttribute("src" ,"images/dice4.png");
 }
  else if (randomNumber===5){
-  document.querySelector(".img1").setAttribute("src" , "https://github.com/ecsilon1/dice/blob/main/images/dice5.png");
+  document.querySelector(".img1").setAttribute("src" , "images/dice5.png");
 }
  else if (randomNumber===6){
-  document.querySelector(".img1").setAttribute("src" , "https://github.com/ecsilon1/dice/blob/main/images/dice6.png");
+  document.querySelector(".img1").setAttribute("src" , "images/dice6.png");
 }
 
 
 var secondRandomNumber=Math.floor(Math.random(randomNumber)*6)+1;
 
 if(secondRandomNumber===1){
- document.querySelector(".img2").setAttribute("src" , "https://github.com/ecsilon1/dice/blob/main/images/dice1.png");
+ document.querySelector(".img2").setAttribute("src" , "images/dice1.png");
 }
 
 else if(secondRandomNumber===2){
- document.querySelector(".img2").setAttribute("src" , "https://github.com/ecsilon1/dice/blob/main/images/dice2.png");
+ document.querySelector(".img2").setAttribute("src" , "images/dice2.png");
 }
 else if (secondRandomNumber===3){
- document.querySelector(".img2").setAttribute("src" , "https://github.com/ecsilon1/dice/blob/main/images/dice3.png");
+ document.querySelector(".img2").setAttribute("src" , "images/dice3.png");
 }
 else if (secondRandomNumber===4){
- document.querySelector(".img2").setAttribute("src" , "https://github.com/ecsilon1/dice/blob/main/images/dice4.png");
+ document.querySelector(".img2").setAttribute("src" , "images/dice4.png");
 }
 else if (secondRandomNumber===5){
- document.querySelector(".img2").setAttribute("src" , "https://github.com/ecsilon1/dice/blob/main/images/dice5.png");
+ document.querySelector(".img2").setAttribute("src" , "images/dice5.png");
 }
 else if (secondRandomNumber===6){
- document.querySelector(".img2").setAttribute("src" , "https://github.com/ecsilon1/dice/blob/main/images/dice6.png");
+ document.querySelector(".img2").setAttribute("src" , "images/dice6.png");
 }
 
 
